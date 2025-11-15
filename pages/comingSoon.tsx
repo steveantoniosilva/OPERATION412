5a4e1b5139e85c213eb50788d211ea6ab1a20e62
Steve Silva <steveantoniosilva@gmail.com>
2025-11-15 17:53:15 -0500

    z

--- pages/comingSoon.tsx
+++ pages/comingSoon.tsx
@@ -19,31 +19,7 @@ const ComingSoon = () => {
       />
       <div className='main'>
         <div className='container'>
-          <Book
-            title="It's Time to Quarterback Your Marriage"
-            subtitle='Calling Men to Win the Game That Matters Most'
-            img='/quarterback.png'
-          />
-          <Book
-            title="It's Time to Come Out of the Caves"
-            subtitle='Calling Prophets to Come Out of Hiding'
-            img='/cave.png'
-          />
-          <Book
-            title="It's Time to Wake Up Out of Your Sleep"
-            subtitle='Calling Watchmen to Wake Up, Watch and Pray'
-            img='/eagle.png'
-          />
-          <Book
-            title="It's Time to Hunt Wolves Out of the Church"
-            subtitle='Calling the Church to Discern Wolves'
-            img='/wolf.png'
-          />
-          <Book
-            title="It's Time to Dream Dreams from God"
-            subtitle="Calling Dreamers to know God's Language of Dreams"
-            img='/jacobs-ladder.png'
-          />
+<h1>coming soon</h1>
           <div style={{ marginBottom: '333px' }}></div>
         </div>
       </div>
--- pages/currentBooks.tsx
+++ pages/currentBooks.tsx
@@ -22,28 +22,28 @@ const Books = () => {
         <div className='container'>
           <Book
             title="It's Time to Quarterback Your Marriage"
-            subtitle='A Call to Men to Win the Game That Matters Most'
+            subtitle='Calling Men to Win the Game That Matters Most'
             img='/quarterback.png'
           />
           <Book
             title="It's Time to Come Out of the Caves"
-            subtitle="A Call to Prophets to Come Out of Hiding"
+            subtitle='Calling Prophets to Come Out of Hiding'
             img='/cave.png'
           />
           <Book
             title="It's Time to Wake Up Out of Your Sleep"
-            subtitle="A Call to Watchmen to Wake Up, Watch and Pray"
+            subtitle='Calling Watchmen to Wake Up, Watch and Pray'
             img='/eagle.png'
           />
           <Book
-            title="It's Time to Hunt Wolves Out of the Church"
-            subtitle='A Call to Identify Wolves Who Wear Wool'
+            title="It's Time for Sheep to Hunt Wolves"
+            subtitle='Calling Sheep to Confront Wolves in the Church'
             img='/wolf.png'
           />
           <Book
-            title="It's Time to Encounter God in Dreams"
-            subtitle="A Call to Meditate on Every Dream In the Bible"
-            img='/jacobs-ladder.png'
+            title="It's Time for An Outpouring of Dreams"
+            subtitle="Calling Dreamers to Meditate on Every Dream in the Bible"
+            img='/statue.png'
           />
           <div style={{ marginBottom: '333px' }}></div>
         </div>
