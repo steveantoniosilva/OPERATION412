b0265cd4a45e388d51024a6097105d071319e284
Steve Silva <steveantoniosilva@gmail.com>
2025-11-18 09:06:24 -0500

    z

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,12 @@ import { Twirl as Hamburger } from 'hamburger-react';
 import { useClickAway } from 'react-use';
 import styles from '../styles/navbar.module.css';
 import { useRouter } from 'next/router';
+import { Lexend } from 'next/font/google';
+
+const navText = Lexend({
+  subsets: ['latin'],
+  weight: ['100'],
+});
 
 function Navbar() {
   const router = useRouter();
@@ -20,8 +26,8 @@ function Navbar() {
   };
 
   const navLinks = [
-    { href: '/books', label: 'Books' },
-    { href: '/comingSoon', label: 'Coming Soon' },
+    { href: '/books', label: 'books' },
+    { href: '/comingSoon', label: 'coming soon' },
   ];
 
   return (
@@ -50,7 +56,7 @@ function Navbar() {
                   key={href}
                   className={styles.li}>
                   <Link
-                    className={`${styles.mobileNavLink} ${router.pathname === href ? styles.active : ''}`}
+                    className={`${styles.mobileNavLink} ${navText.className} ${router.pathname === href ? styles.active : ''}`}
                     href={href}
                     onClick={() => isOpen && handleClick()}>
                     {label}
@@ -68,7 +74,7 @@ function Navbar() {
           <Link
             key={href}
             href={href}
-            className={`${styles.desktopNavLink} ${router.pathname === href ? styles.active : ''}`}>
+            className={`${styles.desktopNavLink} ${navText.className} ${router.pathname === href ? styles.active : ''}`}>
             {label}
           </Link>
         ))}
--- pages/books.tsx
+++ pages/books.tsx
@@ -21,33 +21,33 @@ const Books = () => {
       <div className='main'>
         <div className='container'>
           <Book
-            title="Chosen to Quarterback Your Marriage"
+            title='Called to Quarterback Your Marriage'
             subtitle='Calling Men to Win the Game That Matters Most'
             img='/quarterback.png'
           />
           <Book
-            title="Chosen to Be A Prophet &"
+            title='Called to Be A Prophet'
             subtitle='Calling Prophets to Come Out of Hiding'
             img='/cave.png'
           />
           <Book
-            title="Chosen to Wake Up Out of Your Sleep"
+            title='Called to Be a Watchman'
             subtitle='Calling Watchmen to Wake Up, Watch and Pray'
             img='/eagle.png'
           />
           <Book
-            title="Chosen to Confront Wolves in the Church"
+            title='Called to Confront Wolves in the Church'
             subtitle='Calling Sheep to Discern Wolves in Wool'
             img='/wolf.png'
           />
           <Book
-            title="Chosen to Receive & Interpret Dreams"
-            subtitle="Calling Dreamers to Understand the Language of Dreams"
-            img='/statue.png'
+            title='Called to See & Interpret Dreams'
+            subtitle='Calling Dreamers to Understand the Language of Dreams'
+            img='/daniels-statue.png'
           />
           <Book
-            title="Chosen to Receive & Interpret Visions"
-            subtitle="Calling Seers to Understand the Language of Visions"
+            title='Called to See & Interpret Visions'
+            subtitle='Calling Seers to Understand the Language of Visions'
             img='/leopard.png'
           />
           <div style={{ marginBottom: '333px' }}></div>
