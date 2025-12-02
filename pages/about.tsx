a6b162c1d35021178539bd5b7196fc47c9e157cc
Steve Silva <steveantoniosilva@gmail.com>
2025-12-02 04:10:23 -0500

    z

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ const titleFont = Poppins({
 
 const textFont = Cormorant_Garamond({
   subsets: ['latin'],
-  weight: ['300'],
+  weight: ['300', '700'],
   style: ['normal', 'italic'],
 });
 
@@ -18,17 +18,57 @@ const About = () => {
   return (
     <div className='main'>
       <div className='container'>
-        <h2 className={`${styles.title} ${textFont.className}`}>
-          Mission <span className={`${styles.title} ${titleFont.className}`}> 4 1 2 </span>
-        </h2>
+        <h2 className={`${styles.title} ${textFont.className}`}>The Mission</h2>
+        <br />
         <h5 className={`${styles.subtitle} ${textFont.className}`}>
-          In Ephesians 4:12, the Holy Spirit articulates through Paul the concept of the “katartismos” of the saints.
-          Katartismos describes the saints being adjusted, aligned, completed, equipped, formed by exercise, framed,
-          furnished, ordered in battle array, instructed, made complete, joined, properly fitted, made good, mended,
-          prepared, put in order, qualified, reconciled, repaired, restored, set in place, set right, and trained—all
-          according to God’s original blueprint for His Church. This is the unseen hand behind my writing—the
-          katartismos of the saints.
+          "…the <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>equipping</span> of the saints…"
         </h5>
+        <h6 style={{ textAlign: 'right', paddingRight: '1em', fontWeight: '100', display: 'inline-block' }}>
+          - Ephesians <h1 style={{ fontWeight: '700' }}>4:12</h1>
+        </h6>
+        <br />
+        <br />
+        <br />
+        <h5 className={`${styles.subtitle} ${textFont.className}`}>
+          "…the <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>equipping</span> of the saints…"
+          <br />
+          <br />
+          In Greek:
+          <br />
+          The word for equipping is <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>katartismos</span> which
+          could be used in the following contexts:
+          <br />
+          <br />
+        </h5>
+        <h5>An army being supplied via dedicated supply lines</h5>
+        <br />
+        <h5>A ship being made ready & prepared for its voyage</h5>
+        <br />
+        <h5>A bone being set back into its proper alignment</h5>
+        <h5 className={`${styles.subtitle} ${textFont.className}`}>
+          <br />
+          This is why I write:
+          <br />
+          <h5 className={`${styles.subtitle} ${textFont.className}`}>
+            To develop supply lines to His Army.
+            <br />
+            To prepare His Body for the Voyage ahead.
+            <br />
+            To see His Body set in alignment.
+          </h5>
+          <br />
+          <br />
+          <h5 className={`${styles.subtitle} ${textFont.className}`}>
+            "…for the <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>equipping</span> of the saints for the
+            work of ministry, for the building up of the body of Christ…"
+          </h5>
+        </h5>
+        <br />
+        <br />
+        <h6 style={{ textAlign: 'right', paddingRight: '1em', fontWeight: '100', display: 'inline-block' }}>
+          - Ephesians <h1 style={{ fontWeight: '700' }}>4:12</h1>
+        </h6>
+        <div style={{ paddingBottom: '222px' }}></div>
       </div>
     </div>
   );
