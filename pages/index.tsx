67756d9a0bf34d07531ec13467df4dcfde0bd34e
Steve Silva <steveantoniosilva@gmail.com>
2025-12-11 17:52:03 -0500

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,26 @@ const Home = () => {
           <div className={styles.stripe}>2</div>
         </div>
         <div className={styles.desktopContainer}>
-          <div className={styles.stripe}>OPE</div>
-          <div className={styles.stripe}>RAT</div>
-          <div className={styles.stripe}>ION</div>
+          <div className={styles.stripe}>
+            <div className={styles.inner}>
+              <div className={styles.operation}>OPE</div>
+              <div className={styles.fourTwelve}>4</div>
+            </div>
+          </div>
+
+          <div className={styles.stripe}>
+            <div className={styles.inner}>
+              <div className={styles.operation}>RAT</div>
+              <div className={styles.fourTwelve}>1</div>
+            </div>
+          </div>
+
+          <div className={styles.stripe}>
+            <div className={styles.inner}>
+              <div className={styles.operation}>ION</div>
+              <div className={styles.fourTwelve}>2</div>
+            </div>
+          </div>
         </div>
       </>
     </>
