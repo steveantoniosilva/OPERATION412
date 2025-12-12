abed9da4e72cc57613ea8d899daf2c57b4a4e2e7
Steve Silva <steveantoniosilva@gmail.com>
2025-12-12 16:51:18 -0500

    z

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,7 +16,7 @@ const About = () => {
         <div className={styles.stripe}>
           <div className={styles.inner}>
             <div className={styles.equipAndBuild}>EQUIP</div>
-            <h1 className={styles.armyAndBody}>HIS ARMY TO</h1>
+            <div className={styles.armyAndBody}>HIS ARMY TO</div>
             <div className={styles.standAndFight}>FIGHT</div>
           </div>
         </div>
@@ -25,7 +25,7 @@ const About = () => {
         <div className={styles.stripe}>
           <div className={styles.inner}>
             <div className={styles.equipAndBuild}>BUILD</div>
-            <h1 className={styles.armyAndBody}>HIS BODY TO</h1>
+            <div className={styles.armyAndBody}>HIS BODY TO</div>
             <div className={styles.standAndFight}>STAND</div>
           </div>
         </div>
@@ -35,3 +35,45 @@ const About = () => {
 };
 
 export default About;
+
+// The Greek word for “EQUIP” was historically used to describe:
+
+// Then a sequence of verb → object pairs, like these:
+
+// MENDING — fishing nets
+
+// SETTING — dislocated bones
+
+// RECONCILING — relationships
+
+// FORMING — by exercising
+
+// READYING — household courtyards
+
+// OUTFITTING — naval warships
+
+// PREPARING — slings
+
+// RESTORING — fallen sinners
+
+// FRAMING — the universe
+
+// BRINGING — order to cities
+
+// POSITIONING — armies for battle
+
+// EQUIPPING — ships with rowers
+
+// Then you mirrored it with oikodome language:
+
+// The Greek word for “BUILD UP” was historically used to describe:
+
+// FORTIFYING — city walls
+
+// REPAIRING — temple stones
+
+// EXPANDING — a household
+
+// RESTORING — a collapsed structure
+
+// STRENGTHENING — a foundation
