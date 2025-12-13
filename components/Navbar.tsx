9d05e3b0e6c8e2f7d72f44d09ab0d378333d33f3
Steve Silva <steveantoniosilva@gmail.com>
2025-12-13 15:06:09 -0500

    Z

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,10 +10,10 @@ export default function Navbar() {
   const navRef = useRef(null);
 
   const navLinks = [
-    { href: '/', label: 'HOME' },
-    { href: '/about', label: 'ABOUT' },
-    { href: '/books', label: 'BOOKS' },
-    { href: '/comingSoon', label: 'WRITE' },
+    { href: '/', label: 'ROUTE BACK TO CAMP' },
+    { href: '/about', label: 'ABOUT OPERATION 412' },
+    { href: '/books', label: 'OPERATION 412 BOOKS' },
+    { href: '/comingSoon', label: 'CURRENTLY IN THE FIRE' },
   ];
 
   return (
