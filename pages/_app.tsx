b3a77f1df21267f7d4fa8cb7536020f9ae964eee
Steve Silva <steveantoniosilva@gmail.com>
2026-01-13 06:04:41 -0500

    z

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>BATTLE CRY BOOKS</title>
+        <title>Battle Cry Books</title>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1'
