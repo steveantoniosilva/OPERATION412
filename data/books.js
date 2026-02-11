c27b065b2c68a193e046e108d9672d0b836c90e3
Steve Silva <steveantoniosilva@gmail.com>
2026-02-11 05:33:08 -0500

    z

--- data/books.js
+++ data/books.js
@@ -30,56 +30,29 @@ export const books = {
   },
 
   // BUSINESSMAN
+  // soldier
+  // Equipped
+  // Watchman
+  // Investor
+  // Runner
+  // Alien
 
-  // I am a soldier
+  //I Am A MoneyMaker - the modern minister has become a modern money machine
 
-  // I AM A TREE????
-
-  // I Am A BATTLE HORSE - I WILL TURN SHEEP INTO MY BATTLE STEED - For the LORD of Hosts cares for His flock, the house of Judah; and He will make them like His majestic horse in battle.
-
-  // I Am Equipped
-
-  // I Am A Watchman
-
-  // I Am An Investor
-
-  // I Am A Runner
-
-  // I Am An Alien
-
-  //I Am A MoneyMaker - the modern minister has become a modern money maching
-
-  // Built Like A An Eagle
-
-  // I Am A Lion / Built Like A lion?
-
-  // I am a Visioner
-
-  // I am An ant
-
-  // I Am a deceiver
-
-  // I Am clay
-
-  // I Am A sheep
-
-  // I Am An Ambassador
-
-  // I am a shofar
-
+  // Eagle
+  // Visioner
+  // ant
+  // clay
+  // sheep
+  // Ambassador
+  // shofar
   // I am gifted -wired for wonder as I'm writing
-
-  // I am a witch? I am into witchcraft? witchcraft is as the sin of rebellion? is as?
-
-  // I am an equipper - Eph 4:12
-  // I am a worker - Eph 4:12
-  // I am a builder - Eph 4:12
-
-  // I am a rapper
-
+  // witchcraft? witchcraft is as the sin of rebellion? is as?
+  // equipper - Eph 4:12
+  // worker - Eph 4:12
+  //builder - Eph 4:12
+  //rapper
   // GOD'S OPERATING SYSTEM?????
-
   // I'm building a company
-
-  // America's Got TALENT? talent?
+  // talent?
 };
